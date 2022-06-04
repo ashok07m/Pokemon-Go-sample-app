2a3dc1d5f286339225dbcf3763791046f77e33d0
Ashok Kumar <ashok07m@gmail.com>
2022-06-04 17:31:31 +0530

    refactor: update function access to private

--- app/src/main/java/com/ashok/pokemongo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/ashok/pokemongo/ui/main/MainViewModel.kt
@@ -24,7 +24,7 @@ class MainViewModel @Inject constructor(
         loadPokemonList()
     }
 
-    fun loadPokemonList() {
+    private fun loadPokemonList() {
         pagingDataFlow = getPokemonListUseCase.invoke().cachedIn(viewModelScope)
     }
 }
